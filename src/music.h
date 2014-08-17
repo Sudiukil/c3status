19bc3dcb69c716c26f45f6987712a43dec836b80
Sudiukil <sudiukil@gmx.fr>
2014-08-17 14:14:37 +0200

    Moved moc.h and ncmpcpp.h content to music.h

--- src/music.h
+++ src/music.h
@@ -23,3 +23,36 @@ char *get_ncmpcpp_infos() {
 		return infos;
 	}
 }
+
+int is_moc_music_played() {
+
+	char *infos = calloc(8, sizeof(char));
+
+	FILE *mocp_stdout = popen("mocp -Q \"%state\" 2> /dev/null", "r");
+
+	fgets(infos, 8, mocp_stdout);
+
+	pclose(mocp_stdout);
+
+	if(infos[0]!='S') return 1;
+	else return 0;
+}
+
+char *get_moc_infos() {
+
+	char *infos = calloc(256, sizeof(char));
+
+	FILE *mocp_stdout = popen("mocp -Q \"%n. %t - %a (%A)\" 2> /dev/null", "r");
+
+	fgets(infos, 256, mocp_stdout);
+
+	infos[strlen(infos)-1] = '\0';
+
+	pclose(mocp_stdout);
+
+	if(strlen(infos)!=0 && is_moc_music_played()) return infos;
+	else {
+		strcpy(infos, "stopped");
+		return infos;
+	}
+}
