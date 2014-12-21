7cd8f119d7d0ff735ec7969fc70978a6f0ce52c0
Sudiukil <sudiukil@gmx.fr>
2014-12-21 18:03:43 +0100

    Updated config.example.h

--- config.example.h
+++ config.example.h
@@ -1,32 +1,39 @@
-// Object to configure and display
+/* c3status sample config file */
+
+/* Items' declaration
+ * Each item to be displayed on the status bar have to be declared here with the following syntax:
+ * <item_type> <item_name>;
+ * Some examples below, see man page for type list */
 date d;
 volume v;
-battery b;
 mem m;
 cpu c;
 fs home;
 fs root;
-wireless w;
 net n;
 music mu;
 
-// Objects configuration, will only be called once
+/* Global & items' configuration
+ * Every attribute of every item have a default value that will be used if the value is not user-defined here
+ * Syntax is the following:
+ * <item_name>.<attribute> = <value>;
+ * <value> is either a numerical value (e.g: 1), a simple quoted character (e.g: 'a') or a double quoted string (e.g: "abc)
+ * See man page for per-item full attribute list and more infos */
 void conf(void) {
 
+	/* These are global c3status options (i.e: they are not attributes to an item)
+	 * See man page for full list */
 	label_color = "#767676";
 	data_color = "#cccccc";
 
+	/* These are items' configuration */
 	d.format = "%d/%m/%Y - %H:%M:%S";	
-
-	b.label = "BAT";
 
 	c.load_pcent_enable = 1;
 	c.temperature_enable = 1;
 
 	n.interface = "enp4s0";
 	n.unit = 'o';
-
-	w.interface = "enp4s0";
 
 	home.mount_point = "/home";
 	root.mount_point = "/";
@@ -35,17 +42,17 @@ void conf(void) {
 	mu.label = "♪";
 }
 
-// What to display in the status bar
+/* Display of items
+ * Syntax is the following:
+ * display_<item_type>(&<item_name>); */
 void update(void) {
 
 	display_music(&mu);
 	display_fs(&root);
 	display_fs(&home);
-	display_wireless(&w);
 	display_net(&n);
 	display_cpu(&c);
 	display_mem(&m);
-	display_battery(&b);
 	display_volume(&v);
 	display_date(&d);
 }
